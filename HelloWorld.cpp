2feccb3ebcfaeebcccadd30d489a7f59e2af5a35
The Only Devil <33813268+pranav7955@users.noreply.github.com>
2018-10-28 18:45:33 +0530

    Updated helloworld.cpp with some new improvements

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout <<"\nHello World"<< endl;
+    string str;
+    cout << "\nHello World.." << endl;
+    cout << "What is your name ?\n" ;
+    getline (cin, str);
+    cout << "Hello, " << str ;
+    
     return 0;
 }
